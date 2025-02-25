5451e8b172b2be8e5ff08671125e714f5cf6ff46
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-02-25 13:31:07 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,10 +1,40 @@
-num = 100
-data = "hello world"
+# variable => names => identifiers
+
+# firstnum = 100
+# cannot use space in a variable name
+
+# alphabets , _, digits
+
+# num1st = 89
+# digits cannot be t he first letter of tje variable
+
+# num = 90
+# Num = 80
+# print(Num)
+# small upper case are treated as different characyers
+# python language case sensitive lang
+
+num = 80
+print(type(num))
+num = "helllo "
+print(type(num))
+num = 98.78
+print(type(num))
+# variables inside python are dynamically typed
+
+
+# describe the code
+# num = 100
+# data = "hello world"
+# comment
 # num = variable
+"""
+multi line comment
 print(num)
 print(data)
-# print(100)
-# print(type('hello world'))
+print(100)
+print(type('hello world'))
+"""
 
 # "hello world"
 # "iam a person"
