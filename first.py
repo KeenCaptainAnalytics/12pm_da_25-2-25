18d656aa86aa6fa33bbe0c277e60d2605c2984a7
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-02-25 12:34:55 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,29 @@
+print(type(56.34))
+print(type(100))
+print(type("textual data"))
+
+
+
+
+
+# number 100
+# string "textual data "
+
+# number  = > fractional,  integer
+
+# floating type =>23.5
+# integer = > 45
+
+
+
+# data analytics = > 
+
+
+
+# we are using programming languauges -  prog languauges can talk to the processor directly
+
+# processor => has a very high processing power 
+# if we complete a task of reading a file in 1 day then the processor can do it within a few minutes
+
+
+# laptops  => memory processing 
