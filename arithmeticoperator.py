67ce59a29c694a5bbe8c3bc81c883287f4511f7f
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-02-26 12:41:39 +0530

    Create arithmeticoperator.py

---
+++ arithmeticoperator.py
@@ -0,0 +1,74 @@
+# operators
+
+# arithmetic operators
+# + - / * // % **
+
+num1 = 2
+num2 = 3
+pow =  num1**num2 # 2**3 = > 2*2*2
+print(pow)
+
+# num1 = 192
+# num2 = 10
+
+# div = num1/num2
+# print(div)
+
+# divf = num1//num2 # floor division
+# print(divf)
+
+# modDiv = num1 % num2 
+# print(modDiv)
+
+
+
+# science = 45
+# maths =55
+
+# sum = science + maths
+# print("sum is  : ", sum)
+
+# diff = science - maths
+
+# print("diff of the two = ", diff)
+
+
+
+# num1 = 40
+# num2 = 45
+
+# num1 + num2
+
+
+# num1 =90 
+# num2 =45
+# num3= 56
+# num4 =78
+# num5 =23
+
+# num = [90, 45, 56, 78, 23]
+ 
+# typenum = type(num)
+# print(typenum)
+
+
+# stu1Marks = 13
+# stu2Marks = 10
+
+# stu2Marks = stu2Marks + 2
+
+
+
+# variables = > storage spaces 
+# named storage spaces
+# num = 100
+# print(num)
+# num = num +6
+#  num = num/200 *100
+
+# 34 => int
+# "kdmfskmdkmfds"
+# 'skdfgskdfgksdf'
+# 34.45 => float
+
+#print("hello world")
