eb661d7fbc3680b8ff2931ab69c12c33c00a4e02
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-02-26 13:13:20 +0530

    Create conditionaloperator.py

---
+++ conditionaloperator.py
@@ -0,0 +1,61 @@
+# logical operators
+#  and or not
+
+# print( True and True)
+# print( True and  False)
+# print( False and True)
+# print( False and False )
+
+# operand 1  and  operand2  answer
+# true             true     true
+# true             false    false
+# fasle            true     false
+# false            false    false
+
+
+# operand1 or operand2  output
+# True        True       True
+# True        False      True
+# False       True       True
+# False       False      False 
+
+# print(True    or    True  )
+# print(True    or     False)
+# print(False   or    True)
+# print(False   or   False )
+
+
+print(not False)
+result is inverted in case of not operator
+
+
+
+
+
+
+
+
+
+
+
+
+# rain  true / false
+# umbrella true / false
+# # go out 
+
+# is it raining?  do we have an umbrella ? =>  out?
+# true             true                       true
+# true             fasle                      false
+# false            true                       true
+# false            false                      true
+
+
+
+# comparision operators
+# < > <= >= == !=
+
+# num1 = 45
+# num2 = 78
+
+# result = num1 != num2
+# print(result)
