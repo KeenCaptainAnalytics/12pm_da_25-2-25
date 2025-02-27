5f9c67cdd9b539123860c07f79bd5502105fae0b
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-02-27 12:53:11 +0530

    Update problemsOperators.py

--- problemsOperators.py
+++ problemsOperators.py
@@ -18,9 +18,38 @@
 # # isOdd = (num % 2 ) ==1
 # print("is the number odd? : ", not isEven)
 
-Q = > num => divisible by 2 3 5 all
+# Q = > num => divisible by (2 and 3) or  (2 and 5) 
+# 1: is my number divisible by either (2 and 3) or  (2 and 5)?
+# 2: 1: is my number not divisible by either (2 and 3) or (2 and 5)?
+
+a = input("enter a number : ")
+num = int(a)
+
+isDiv = ((num % 2 ==0 ) and    (num % 3 == 0) )
+                        or
+        ((num % 2 == 0) and (num % 5 == 0))
+
+print("is my number divisible by 2 and 3 or 2 and 5? : ", isDiv)
+print("is my num not divisible by 2 and 3 or 2 and  5? : ", not isDiv)
 
 
+
+
+# Q = > num => divisible by 2 3 5 all
+# 1: is my number divisible by 2 3 and 5?
+# 2: is my num not divisible by 2 3 and 5?
+
+# a = input("enter a number : ")
+# num = int(a)
+
+# isDiv = (num % 2 ==0 ) and    (num % 3 == 0) and (num % 5 == 0)
+
+# print("is my number divisible by 2 3 and 5? : ", isDiv)
+# print("is my num not divisible by 2 3 and 5? : ", not isDiv)
+
+
+# True True True => True 
+# True False True = > False
 
 
 
