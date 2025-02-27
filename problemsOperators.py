0bad27c6c22ada3503b5326c44d1ba94d01db4c3
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-02-27 12:29:39 +0530

    Create problemsOperators.py

---
+++ problemsOperators.py
@@ -0,0 +1,32 @@
+# logical operators => and or not  comparision
+
+# Q= > take an input from user and tell if its an odd number 
+#     2> even number 
+    
+#     num = 56
+#     even ? True
+#     odd?   False
+    
+# num = 23+34+23
+
+# num = input("enter some number  :  ") 
+# num = int(num)
+
+# isEven = (num % 2) == 0
+# print("is the number even? : ", isEven)
+
+# # isOdd = (num % 2 ) ==1
+# print("is the number odd? : ", not isEven)
+
+Q = > num => divisible by 2 3 5 all
+
+
+
+
+
+
+
+
+
+
+
