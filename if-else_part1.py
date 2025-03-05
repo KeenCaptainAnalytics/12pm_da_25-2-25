ca101c5c117e8f108ea48d7e0f9e18f3c55f2166
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-03-05 12:40:05 +0530

    Create if-else_part1.py

---
+++ if-else_part1.py
@@ -0,0 +1,17 @@
+# q => input from user of a number 
+# greater than or not
+
+
+# bday = input("is it your birthday today?(y/n) : ")
+
+# if (bday == 'y'):
+#     print("hey today is your birthday")
+#     print("very happy birthday to you")
+# else:
+#     print("oh its not your birthday ")
+#     print("i will wish you on you birthday")
+
+# if(){
+#     line 1
+#     line 2
+# }
