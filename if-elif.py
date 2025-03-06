59fb9b39ba83bb70d093a53631156c4238a6c969
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-03-06 12:26:48 +0530

    Create if-elif.py

---
+++ if-elif.py
@@ -0,0 +1,47 @@
+# input => weekday
+# mon => blue tues= > yellow =
+
+weekday =  input("which day you want to know about? : ")
+
+if (weekday == "monday") :
+    print("blue dress")
+    print("today is martin's birthday")
+elif (weekday == "tuesday"):
+    print("pink dress")
+    print("today is arun's birthday")
+elif (weekday == "wednesday") :
+    print("red dress")
+    print("nobody is having their bday today")
+elif (weekday == "thursday") :
+    print("yellow dress")
+    print("riya's birthday")
+elif( weekday == "friday"):
+    print("brown dress")
+    print("tara bday")
+elif( weekday == "saturday"):
+    print("purple dress")
+    print("nones bday")
+elif (weekday == "sunday"):
+    print("black dress")
+    print("pulkit's bday")
+else :
+    print("wrong input case")
+
+
+print("the program has executed successfully")
+
+
+# num =91
+
+
+
+#
+# if (num%2) == 0 :
+#     print("even")
+#     print("even again ")
+# else :
+#     print("odd")
+#
+# print("hello there!!")
+
+
